c9bd13a7c08d72e4a27edfd993b66f424f6933ed
Areg Abgaryan <aabgaryan@vmware.com>
2023-08-20 03:30:35 +0400

    Refactor QuizApplication class

--- src/main/java/com/areg/project/QuizApplication.java
+++ src/main/java/com/areg/project/QuizApplication.java
@@ -7,7 +7,8 @@ package com.areg.project;
 import com.areg.project.managers.AuthenticationManager;
 import com.areg.project.managers.FileParsingManager;
 import com.areg.project.managers.QuizWorkflowManager;
-import com.areg.project.managers.UserManager;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -19,20 +20,31 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
  * @version 1.0
  * @author Areg Abgaryan
  */
-@SpringBootApplication
-@EntityScan
-//  FIXME !! Init in Autowired constructors all Spring beans
 //  FIXME !! After authentication, choose play game or see my info or see my records
 //  FIXME !! Consider authenticating in a loop, sign out, sign in maybe
+@SpringBootApplication
+@EntityScan
 public class QuizApplication {
-    private static final FileParsingManager fileParsingManager = new FileParsingManager();
-    private static final String dataFilesPath = fileParsingManager.getDataFilesDirectory();
-    private static final UserManager userManager = new UserManager();
-    private static final AuthenticationManager authenticationManager = new AuthenticationManager(userManager);
-    private static final QuizWorkflowManager quizWorkflowManager = new QuizWorkflowManager();
+
+    private final FileParsingManager fileParsingManager;
+    private final AuthenticationManager authenticationManager;
+    private final QuizWorkflowManager quizWorkflowManager;
+
+    @Autowired
+    public QuizApplication(FileParsingManager fileParsingManager, AuthenticationManager authenticationManager,
+                           QuizWorkflowManager quizWorkflowManager) {
+        this.fileParsingManager = fileParsingManager;
+        this.authenticationManager = authenticationManager;
+        this.quizWorkflowManager = quizWorkflowManager;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(QuizApplication.class, args);
+    }
+
+    @PostConstruct
+    public void initQuiz() {
+        final String dataFilesPath = fileParsingManager.getDataFilesDirectory();
         fileParsingManager.parseDataFiles(dataFilesPath);
         authenticationManager.authenticate();
         quizWorkflowManager.initQuiz();
